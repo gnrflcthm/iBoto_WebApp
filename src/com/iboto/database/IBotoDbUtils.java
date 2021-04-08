d132643c68f5d5789888fbd9e60283d3477ac736
Gian Thiam <70900049+gnrflcthm@users.noreply.github.com>
2021-04-08 17:31:19 +0800

    Added more functionality to DbUtils.

--- src/com/iboto/database/IBotoDbUtils.java
+++ src/com/iboto/database/IBotoDbUtils.java
@@ -1,7 +1,6 @@
 package com.iboto.database;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -16,34 +15,49 @@ public class IBotoDbUtils {
 	
 	private final String USER;
 	private final String PASSWORD;
+	private final String DRIVER;
 	private static String URL = "jdbc:mysql://localhost:3306/evotingdb";
 	private Connection conn;
 	
 	public IBotoDbUtils(ServletContext context) {
 		this.USER = context.getInitParameter("user");
 		this.PASSWORD = context.getInitParameter("password");
+		this.DRIVER = context.getInitParameter("driver");
+	}
+	
+	public static IBotoDbUtils getInstance(ServletContext context) {
+		return new IBotoDbUtils(context);
 	}
 	
 	public boolean addUser(String email, String phoneNum, String lastName, String firstName, String birthday, City address, int district, String password) {
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			conn = DriverManager.getConnection(URL, USER, PASSWORD);
-			String userID = generateUserID(address);
-			String[] pwd = HashUtils.hash(password);
-			PreparedStatement s = conn.prepareStatement("INSERT INTO voter(UserID, Email, PhoneNumber, LastName, FirstName, DateOfBirth, Address, District, UPassword, Password_Salt)" +
-					"VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-			s.setString(1, userID);
-			s.setString(2, email);
-			s.setString(3, phoneNum);
-			s.setString(4, lastName);
-			s.setString(5, firstName);
-			s.setString(6, birthday);
-			s.setString(7, address.getProperName());
-			s.setInt(8, district);
-			s.setString(9, pwd[0]);
-			s.setString(10, pwd[1]);
-			
-			s.executeUpdate();			
+			if (connect()) {	
+				String userID = generateUserID(address);
+				String[] pwd = HashUtils.hash(password);
+				String sql = "INSERT INTO voter(UserID, Email, PhoneNumber, LastName, FirstName, DateOfBirth, Address, District, UPassword, Password_Salt)" +
+							 "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+				PreparedStatement s = conn.prepareStatement(sql);
+				s.setString(1, userID);
+				s.setString(2, email);
+				s.setString(3, phoneNum);
+				s.setString(4, lastName);
+				s.setString(5, firstName);
+				s.setString(6, birthday);
+				s.setString(7, address.getProperName());
+				s.setInt(8, district);
+				s.setString(9, pwd[0]);
+				s.setString(10, pwd[1]);
+				
+				s.executeUpdate();
+				
+				if (s != null && !s.isClosed()) {
+					s.close();
+				}
+				
+				disconnect();
+			} else {
+				return false;
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
@@ -51,10 +65,39 @@ public class IBotoDbUtils {
 		return true;
 	}
 	
+	public boolean validateUserLogin(String user, String password) {
+		String[] pwd = new String[2];
+		try {
+			if (connect()) {
+				String sql = "SELECT UPassword, Password_Salt FROM voter WHERE UserID = ? OR Email = ?";
+				PreparedStatement s = conn.prepareStatement(sql);
+				s.setString(1, user);
+				s.setString(2, user);
+				ResultSet res = s.executeQuery();
+				res.next();
+				pwd = new String[] {res.getString(1), res.getString(2)};
+				if (res != null && !res.isClosed()) {
+					res.close();
+				}
+				if (s != null && !s.isClosed()) {
+					s.close();
+				}
+				disconnect();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return HashUtils.validateText(pwd[0], password, pwd[1]);
+	}
+	
 	private String generateUserID(City city) throws SQLException {
 		ResultSet res = conn.createStatement().executeQuery("SELECT COUNT(*) FROM voter WHERE UserID LIKE '" + city.getCityCode() + "%'");
 		res.next();
 		int userCount = res.getInt(1);
+		if (res != null && !res.isClosed()) {
+			res.close();
+		}
 		return generateID(city, "V", userCount);
 	}
 	
@@ -69,5 +112,24 @@ public class IBotoDbUtils {
 		StringBuilder id = new StringBuilder();
 		id.append(city.getCityCode() + type + String.format("%06d", count));
 		return id.toString();
+	}
+	
+	private boolean connect() {
+		try {		
+			Class.forName(DRIVER);
+			conn = DriverManager.getConnection(URL, USER, PASSWORD);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean disconnect() throws SQLException {
+		if (conn != null && !conn.isClosed()) {
+			conn.close();
+			return true;
+		}
+		return false;
 	}
 }

c5edd3af8858bce4946cb3fade7e4871e0a64bc6
Gian Thiam <70900049+gnrflcthm@users.noreply.github.com>
2021-05-19 18:37:01 +0800

    Added class for handling sign out events.

---
+++ src/com/iboto/servlet/SignOutServlet.java
@@ -0,0 +1,22 @@
+package com.iboto.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SignOutServlet
+ */
+@WebServlet("/signout")
+public class SignOutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getSession().invalidate();
+		request.getRequestDispatcher("home.jsp").forward(request, response);
+	}
+
+}

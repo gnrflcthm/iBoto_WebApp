21329295ad2d06ad2f6817e8090ae96e6280673c
Gian Thiam <70900049+gnrflcthm@users.noreply.github.com>
2021-05-19 18:34:50 +0800

    Modified to be able to handle different data requests.

--- src/com/iboto/servlet/UserServiceServlet.java
+++ src/com/iboto/servlet/UserServiceServlet.java
@@ -2,32 +2,106 @@ package com.iboto.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
 import com.iboto.database.IBotoDbUtils;
+import com.iboto.models.UserBean;
 
 @WebServlet("/service")
 public class UserServiceServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	private IBotoDbUtils db;
+	private Gson gson;
 	
-	public UserServiceServlet() {
-		db = new IBotoDbUtils(this.getServletContext());
+	public void init() {
+		this.db = new IBotoDbUtils(this.getServletContext());
+		this.gson = new Gson();
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String service = request.getParameter("service");
-		PrintWriter out = response.getWriter();
-		response.setContentType("application/json");
 		switch(service) {
 			case "election":
-				
+				loadElections(request, response);
+				break;
+			case "voteCheck":
+				hasVoted(request, response);
+				break;
+			case "candidates":
+				loadCandidates(request, response);
+				break;
+			case "vote":
+				submitVote(request, response);
+				break;
 		}
 	}
+	
+	private void loadElections(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		response.setContentType("application/json");
+		UserBean user = db.getUserInstance(request.getParameter("uid"));
+		out.println(gson.toJson(db.getElections(user.getCityAddress())));
+		out.flush();
+	}
+	
+	private void hasVoted(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		response.setContentType("application/json");
+		String userID = request.getParameter("uid");
+		String electionID = request.getParameter("electionID");
+		Date dateVoted = db.getVoted(userID, electionID);
+		if (dateVoted == null) {
+			out.println(String.format("{\"voted\": \"%s\"}", false));			
+		} else {
+			out.println(String.format("{\"voted\": \"%s\", \"dateVoted\": \"%s\"}", true, dateVoted));
+		}
+		out.flush();
+	}
+	
+	private void loadCandidates(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		response.setContentType("application/json");
+		String userID = request.getParameter("uid");
+		String electionID = request.getParameter("electionID");
+		UserBean user = db.getUserInstance(userID);
+		int district = user.getDistrict();
+		int councilorCount = user.getCityAddress().councilorCount() / user.getCityAddress().getDistrictCount();
+		out.println(gson.toJson(db.getCandidates(electionID, district, councilorCount)));
+		out.flush();
+	}
+	
+	private void submitVote(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		response.setContentType("application/json");
+		List<String> candidateIDs = new ArrayList<>();
+		String electionID = request.getParameter("electionID");
+		String userID = request.getParameter("userID");
+		Collections.list(request.getParameterNames()).stream().forEach((x) -> {
+			if (x.contains("id")) {
+				candidateIDs.add(request.getParameter(x));
+			} 
+		});
+		String referenceNumber = db.castVote(candidateIDs, electionID, userID);
+		
+		if (referenceNumber != null) {
+			out.println(String.format("{\"status\": \"success\", \"referenceNumber\": \"%s\"}", referenceNumber));
+			out.flush();
+		} else {
+			out.println("{\"status\": \"error\"}");
+			out.flush();
+		}
 
+	}
 }
